1acdd880f4f3d3806efb55a60e95c54fab3e8c4b
Victor <victoreverend@gmail.com>
2020-09-19 10:29:28 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,70 @@
+<p align="center">
+  <img src="img/logo-alt.png" width="150" alt="Ga$tando">
+</p>
+
+## 📋 Índice
+
+- [Sobre](#-Sobre)
+- [Tecnologias utilizadas](#-Tecnologias-utilizadas)
+- [Como executar o projeto](#-Como-executar-o-projeto)
+- [Preview](#-Preview)
+
+---
+
+## 🖥 Preview 
+
+<p align="center">
+  <img src="img/preview/registro.png" width="700" alt="Registro de Despesa">
+  <img src="img/preview/consulta.png" width="700" alt="consulta de Despesa">
+</p>
+
+---
+
+## 📖 Sobre 
+
+Este é um spin-off de um projeto desenvolvido durante o curso Desenvolvimento Web Completo, criado pelo Jorge Sant Ana e Jamilton Damasceno e disponibilizado na Udemy.
+
+A proposta original do projeto é uma aplicação que permita ao usuário registrar as suas despesas e armazená-las no localStorage do navegador. O usuário também poderá, eventualmente, consultar e/ou remover suas despesas
+
+Meu objetivo com este spin-off é, além de colocar em prática os conhecimentos adquiridos na aula do curso, implementar novas features para a aplicação e publicá-las no GitHub como forma de evoluir meu código e compartilhá-lo.
+
+---
+
+## 🚀 Tecnologias utilizadas
+
+O projeto está desenvolvido utilizando as seguintes tecnologias:
+
+<span>
+  <img src="https://ik.imagekit.io/xksv/html5_RcOQBX1jU9.png" width="100" alt="HTML">
+</span> 
+
+<span>
+  <img src="https://ik.imagekit.io/xksv/css3_4gA2wiFNCA8.png" width="100" alt ="CSS">
+</span>
+
+<span>
+  <img src="https://ik.imagekit.io/xksv/bootstrap_5A0Id--hY.png" width="100" alt ="Bootstrap">
+</span>
+
+<span>
+  <img src="https://ik.imagekit.io/xksv/javascript_rSYR4yAOvgB.png" width="100" alt ="Javascript">
+</span>
+
+---
+
+## ⌨ Como executar o projeto
+
+```bash
+# Clonar o repositório
+git clone https://github.com/vtorugo/gastando
+
+# Executar o index.html contido no diretório
+
+# Registrar suas despesas
+
+# Consultar ou Remover suas despesas
+```
+
+---
+
+Desenvolvido com amor e raiva por Victor Hugo
---
+++ js/app.js
@@ -0,0 +1,215 @@
+class Expense {
+    constructor(year, month, day, category, description, amount) {
+        this.year = year
+        this.month = month
+        this.day = day
+        this.category = category
+        this.description = description
+        this.amount = amount
+    }
+
+    validateData() {
+
+        for(let i in this) {
+
+            if(this[i] == undefined || this[i] == '' || this[i] == null) {
+                return false
+            }       
+        }
+        return true
+    }
+}
+
+class Database {
+
+    constructor() { 
+        let id = localStorage.getItem('id') 
+
+        if(id === null) {
+            localStorage.setItem('id', 0)
+        } 
+    }
+
+    getNextId() {
+        let nextId = localStorage.getItem('id')
+        return parseInt(nextId) + 1
+    }
+
+    storeData(d) {
+        let id = this.getNextId()
+
+        localStorage.setItem(id, JSON.stringify(d))
+        localStorage.setItem('id', id)
+    }
+    
+    recoverAllRegistry() {
+
+        let expenses = []
+
+        let id = localStorage.getItem('id')
+
+        for(let i = 1; i <= id; i++) {
+
+            let expense = JSON.parse(localStorage.getItem(i))
+    
+            if(expense === null) {
+                continue 
+            }
+
+            expense.id = i
+            expenses.push(expense)
+        }
+        
+        return expenses
+    }
+
+    search(expense) {
+        let filteredExpenses = []
+        filteredExpenses = this.recoverAllRegistry()
+
+       
+        console.log(expense)
+        console.log(filteredExpenses)
+
+        if(expense.ano != '') {
+            filteredExpenses = filteredExpenses.filter(d => d.ano == expense.ano)
+        }
+        if(expense.mes != '') {
+            filteredExpenses = filteredExpenses.filter(d => d.mes == expense.mes)
+        }
+        if(expense.dia != '') {
+            filteredExpenses = filteredExpenses.filter(d => d.dia == expense.dia)
+        }
+        if(expense.categoria != '') {
+            filteredExpenses = filteredExpenses.filter(d => d.categoria == expense.categoria)
+        }
+        if(expense.descricao != '') {
+            filteredExpenses = filteredExpenses.filter(d => d.descricao == expense.descricao)
+        }
+        if(expense.valor != '') {
+            filteredExpenses = filteredExpenses.filter(d => d.valor == expense.valor)
+        }
+        return filteredExpenses
+    }
+
+    removeData(id) {
+        localStorage.removeItem(id)
+    }
+}
+
+let db = new Database()
+
+function registerExpense() {
+    
+    let year = document.getElementById('ano')
+    let month = document.getElementById('mes')
+    let day = document.getElementById('dia')
+    let category = document.getElementById('categoria')
+    let description = document.getElementById('descricao')
+    let amount = document.getElementById('valor')
+
+    let expense = new Expense(
+        year.value, 
+        month.value, 
+        day.value, 
+        category.value, 
+        description.value, 
+        amount.value
+    )
+    
+    if(expense.validateData()) {
+
+        db.storeData(expense)
+        document.getElementById('modal-label').className = "modal-header text-success"
+        document.getElementById('modal-heading').innerHTML = 'Dados gravados com successo'
+        document.getElementById('modal-text').innerHTML = 'Sua despesa foi registrada com sucesso!'
+        document.getElementById('modal-dismiss').className = 'btn btn-success'
+        document.getElementById('modal-dismiss').innerHTML = 'Ok'
+
+        $('#dialog-popup').modal('show')
+
+        document.getElementById('categoria').value = ''
+        document.getElementById('descricao').value = ''
+        document.getElementById('valor').value = ''
+
+    } else {
+
+        document.getElementById('modal-label').className = "modal-header text-danger"
+        document.getElementById('modal-heading').innerHTML = 'Erro na gravação'
+        document.getElementById('modal-text').innerHTML = 'Existem campos obrigatórios que não foram preenchidos corretamente'
+        document.getElementById('modal-dismiss').className = 'btn btn-danger'
+        document.getElementById('modal-dismiss').innerHTML = 'Voltar e corrigir'
+
+        $('#dialog-popup').modal('show')
+    }
+}
+
+function loadAllExpenses(expenses = [], filter = false) {
+
+    if(expenses.length == 0 && filter == false) {
+        expenses = db.recoverAllRegistry()
+
+    }
+
+    let expensesList = document.getElementById('expenses-list')
+    expensesList.innerHTML = ''
+
+    expenses.forEach(function(data) {
+        
+        let row = expensesList.insertRow()
+
+        row.insertCell(0).innerHTML = `${data.day}/${data.month}/${data.year}`
+        
+        switch(data.category) {
+            case '1': data.category = 'Alimentação'
+                break
+            case '2': data.category = 'Educação'
+                break
+            case '3': data.category = 'Lazer'
+                break
+            case '4': data.category = 'Saúde'
+                break
+            case '5': data.category = 'Transporte'
+                break
+        }
+        row.insertCell(1).innerHTML = data.category
+
+        row.insertCell(2).innerHTML = data.description
+        row.insertCell(3).innerHTML = `R$ ${data.amount}`
+
+        let btn = document.createElement("button")
+        btn.className = 'btn btn-danger'
+        btn.innerHTML = '<i class="fas fa-minus"></i>'
+        btn.id = `expense_id_${data.id}`
+        console.log(btn.id)
+
+        btn.onclick = function() {
+
+            let id = this.id.replace('expense_id_', '')
+            db.removeData(id)
+            window.location.reload()
+        }
+
+        row.insertCell(4).append(btn)
+
+        console.log(data)
+        
+    })    
+}
+
+function pesquisarDespesa() {
+    
+    let year = document.getElementById('ano').value
+    let month = document.getElementById('mes').value
+    let day = document.getElementById('dia').value
+    let category = document.getElementById('categoria').value
+    let description = document.getElementById('descricao').value
+    let amount = document.getElementById('valor').value
+
+    let expense = new Expense(year, month, day, category, description, amount)
+    
+    let expenses = db.search(expense)
+
+    loadAllExpenses(expenses, true)    
+}   
+
